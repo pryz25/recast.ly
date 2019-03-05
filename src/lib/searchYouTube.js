547d413d5cac60d8cf557b244d1df8950f6e27c8
pryz25 <pryzanych@gmail.com>
2019-03-05 12:48:43 -0800

    Started building search query

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,17 @@
+import YOUTUBE_API_KEY from '../config/youtube.js';
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/videos',
+    type: 'GET',
+    data: {'part': 'id,snippet',
+      'key': options.key,
+      'maxResults': options.max,
+
+    },
+    success: callback
+    
+  });
 };
 
 export default searchYouTube;
