f229c5fa3990866c141d951e003735dbe40dd132
pryz25 <pryzanych@gmail.com>
2019-03-04 21:23:30 -0800

    Most functionality but change video on click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,25 @@
+import VideoList from './VideoList.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoPlayer from './VideoPlayer.js';
+
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: exampleVideoData,
+      video: exampleVideoData[0]
+    };
+  }
+
+  onTitleClick() {
+    this.setState({
+      list: exampleVideoData,
+      video: exampleVideoData[0]
+    });
+    console.log('click');
+  }
+
   render() {
     return (
       <div>
@@ -9,10 +30,10 @@ class App extends React.Component {
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <div><h5><em>videoPlayer</em> view goes here</h5></div>
+            <div><h5><em><VideoPlayer video={exampleVideoData[0]}/></em></h5></div>
           </div>
           <div className="col-md-5">
-            <div><h5><em>videoList</em> view goes here</h5></div>
+            <div><h5><em><VideoList videos={exampleVideoData} click={this.onTitleClick.bind(this)}/></em></h5></div>
           </div>
         </div>
       </div>
